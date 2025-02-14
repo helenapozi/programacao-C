c08c291ed892d796cf46806f218df4286ff8c3f2
helenapozi <helenapozi28@gmail.com>
2025-02-14 21:34:19 +0000

    createtpdadossimples.C

--- ttiposdedados.C
+++ ttiposdedados.C
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main (){
+
+    int a=10;
+    int b=3;
+
+  int soma = a+b;
+  int diferenca = a-b;
+  int produto = a*b;
+  int quociente = a/b;
+
+  printf ("soma:%d\n" , soma);
+  printf("diferenca:%d\n" , diferenca);
+  printf("produto:%d\n" , produto);
+  printf("quociente: %d\n" , quociente);
+
+return 0;
+}
