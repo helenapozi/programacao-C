b877116ebecde0685c458163b3b26a53f7fdbf86
helenapozi <helenapozi28@gmail.com>
2025-02-14 18:58:57 +0000

    create incrementoedecremento

--- incrementoedecremento.C
+++ incrementoedecremento.C
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main (){
+
+int numero1 = 1;
+numero1++;
+
+printf("antes incremento:%d\n", numero1);
+printf("apos incremento:%d\n" , numero1);
+
+numero1 --;
+printf("apos decremento :%d\n" , numero1);
+
+return 0;
+
+
+}
