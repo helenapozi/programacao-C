de2c840e0c4229a56839738fc4ae6174f1aa8969
helenapozi <helenapozi28@gmail.com>
2025-02-14 19:17:48 +0000

    creatatribuiçoes

--- operadores de atribuição.C
+++ operadores de atribuição.C
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main (){
+
+int numero1 = 10, numero2, resultado;
+
+resultado=10;
+printf("resultado:%d\n" , resultado);
+
+resultado+=20;
+printf("resultado:%d\n" , resultado);
+
+resultado-= numero1;
+printf("resultado:%d\n", resultado);
+
+resultado*= 5;
+printf("resultado%d\n", resultado);
+
+resultado/=2;
+printf("resultado:%d\n" , resultado);
+ return 0;
+ }
 
