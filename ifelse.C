70d1adb5e37ed792d69a726754f66fbffa68c483
helenapozi <helenapozi28@gmail.com>
2025-02-26 15:42:35 +0000

    ifelsecodar.C

--- ifelse.C
+++ ifelse.C
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+int main (){
+float temperatura , umidade;
+unsigned int estoque;
+
+
+
+printf("entre com a temperatura\n");
+scanf("%f",&temperatura);
+
+printf("entre com a umidade\n");
+scanf("%f", &umidade);
+
+printf("entre com o estoque\n");
+scanf("%u", &estoque);
+
+
+if(temperatura > 50){
+printf("temperatura esta elevada!\n ");
+}else{
+    printf("temperatura dentro dos parametros");
+}
+
+if (umidade > 70){
+    printf("a umidade esta controlada\n");
+
+ }else{
+    printf("a umidade esta descontrolada\n");
+ }
+
+if (estoque > 100){
+    printf("estoque bem abastecido\n");
+
+}else{
+    printf("estoque baixando\n");
+}
+return 0;
+
+}
